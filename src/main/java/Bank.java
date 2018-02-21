9574f927cd0d3d4e31f7a85627540959997d7b1a
Nicholas Nusgart <nicknusgart@gmail.com>
2018-02-21 12:20:56 -0600

    Implemented lab

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -8,78 +8,18 @@
  * @see <a href="https://cs125.cs.illinois.edu/lab/8/">Lab 8 Description</a>
  */
 public class Bank {
+    /**
+     * The bank's name.
+     */
+    private String bankName;
 
-    public String bankName;
-
+    /**
+     * Construct a default bank.
+     */
     public Bank() {
-        bankName = "Illini Bank";
+        setBankName("Illini Bank");
     }
 
-    /**
-     * Withdraw money from an account.
-     * <p>
-     * Subtracts the amount of money from bank account's balance. Returns true if transaction is
-     * successful, false otherwise.
-     *
-     * @param bankAccount to withdraw money from.
-     * @param amount to withdraw (double)
-     * @return boolean
-     */
-    public boolean withdrawMoney(final BankAccount bankAccount, final double amount) {
-        /*
-         * Implement this function
-         */
-    }
-
-    /**
-     * Deposit money in an account.
-     * <p>
-     * Adds the amount of money to bank account's balance. Returns true if transaction is
-     * successful, false otherwise.
-     *
-     * @param bankAccount to deposit money to.
-     * @param amount to deposit
-     * @return boolean
-     */
-    public boolean depositMoney(final BankAccount bankAccount, final double amount) {
-        /*
-         * Implement this function
-         */
-    }
-
-    /**
-     * Transfer money from one account to another.
-     * <p>
-     * Transfer the amount of money from one back account to another. Returns true if transaction is
-     * successful, false otherwise.
-     *
-     * @param source bank account to transfer money from.
-     * @param destination bank account to transfer money to.
-     * @param amount to transfer
-     * @return boolean
-     */
-
-    public boolean transferMoney(final BankAccount source, final BankAccount destination,
-            final double amount) {
-        /*
-         * Implement this function
-         */
-    }
-
-    /**
-     * Change back account owner name.
-     *
-     * @param bankAccount to change
-     * @param name new name to set
-     */
-
-    public void changeOwnerName(final BankAccount bankAccount, final String name) {
-        /*
-         * Implement this function
-         */
-    }
-
-    public static int totalAccounts = 0;
     /**
      * Uses static variable to get number of bank accounts opened.
      *
@@ -89,6 +29,7 @@ public class Bank {
         /*
          * Implement this function
          */
+        return BankAccount.numberOfAccountsOpened();
     }
 
     /**
@@ -99,7 +40,7 @@ public class Bank {
     @SuppressWarnings("checkstyle:magicnumber")
     public static void main(final String[] unused) {
         Bank bank = new Bank();
-        System.out.println("Welcome to " + bank.bankName);
+        System.out.println("Welcome to " + bank.getBankName());
         System.out.println("We are excited to have you banking with us!\n\n");
 
         // Create Bank Accounts
@@ -120,7 +61,109 @@ public class Bank {
         bank.transferMoney(account2, account1, 350.0);
 
         // Print number of accounts
-        System.out.print("Number of active accounts at " + bank.bankName + " are ");
-        System.out.println(Bank.totalAccounts);
+        System.out.print("Number of active accounts at " + bank.getBankName() + " are ");
+        System.out.println(Bank.getNumberOfAccount());
+    }
+
+    /**
+     * Withdraw money from an account.
+     * <p>
+     * Subtracts the amount of money from bank account's balance. Returns true if transaction is
+     * successful, false otherwise.
+     *
+     * @param bankAccount to withdraw money from.
+     * @param amount to withdraw (double)
+     * @return boolean
+     */
+    public boolean withdrawMoney(final BankAccount bankAccount, final double amount) {
+        /*
+         * Implement this function
+         */
+        if (amount < 0 || !Double.isFinite(amount)) {
+            return false;
+        }
+        if (amount > bankAccount.getAccountBalance()) {
+            return false;
+        }
+        bankAccount.setAccountBalance(bankAccount.getAccountBalance() - amount);
+        return true;
+    }
+
+    /**
+     * Deposit money in an account.
+     * <p>
+     * Adds the amount of money to bank account's balance. Returns true if transaction is
+     * successful, false otherwise.
+     *
+     * @param bankAccount to deposit money to.
+     * @param amount to deposit
+     * @return boolean
+     */
+    public boolean depositMoney(final BankAccount bankAccount, final double amount) {
+        /*
+         * Implement this function
+         */
+        if (amount < 0 || !Double.isFinite(amount)) {
+            return false;
+        }
+        bankAccount.setAccountBalance(bankAccount.getAccountBalance() + amount);
+        return true;
+    }
+
+    //public static int totalAccounts = 0;
+
+    /**
+     * Transfer money from one account to another.
+     * <p>
+     * Transfer the amount of money from one back account to another. Returns true if transaction is
+     * successful, false otherwise.
+     *
+     * @param source bank account to transfer money from.
+     * @param destination bank account to transfer money to.
+     * @param amount to transfer
+     * @return boolean
+     */
+
+    public boolean transferMoney(final BankAccount source, final BankAccount destination,
+            final double amount) {
+        /*
+         * Implement this function
+         */
+        if (source.getAccountBalance() < amount) {
+            return false;
+        }
+        if (amount < 0 || !Double.isFinite(amount)) {
+            return false;
+        }
+        withdrawMoney(source, amount);
+        depositMoney(source, amount);
+        return true;
+    }
+
+    /**
+     * Change back account owner name.
+     *
+     * @param bankAccount to change
+     * @param name new name to set
+     */
+
+    public void changeOwnerName(final BankAccount bankAccount, final String name) {
+        bankAccount.setOwnerName(name);
+    }
+
+    /**
+     * The bank's name.
+     * @return the bank's name.
+     */
+    public String getBankName() {
+        return bankName;
+    }
+
+    /**
+     * Set the bank's name.
+     * @param name the new name.
+     */
+    public void setBankName(final String name) {
+        this.bankName = name;
     }
 }
--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,38 +1,194 @@
-import java.util.Random;
-
 /**
  * Class implementing a bank account.
+ * Auditing not implemented.
  * <p>
  * Complete and document this class as part of Lab 8.
  *
  * @see <a href="https://cs125.cs.illinois.edu/lab/8/">Lab 8 Description</a>
  */
+@SuppressWarnings("checkstyle:publicvariables")
 public class BankAccount {
-
-    /*
-     * You may want to use this to distinguish between different kinds of accounts.
+    /**
+     * The account id counter.
      */
-    public enum BankAccountType {
-        CHECKINGS,
-        SAVINGS,
-        STUDENT,
-        WORKPLACE
-    }
-
-    private int accountNumber;
-    public BankAccountType accountType;
+    private static int accountsCounter = 0;
+    /**
+     * account number.
+     */
+    private final int accountNumber;
+    /**
+     * the Account Type.
+     */
+    private BankAccountType accountType;
+    /**
+     * account balance.
+     */
     private double accountBalance;
+    /**
+     * owner name.
+     */
     private String ownerName;
-    public double interestRate;
+    /**
+     * The Account's current interest Rate.
+     */
+    private double interestRate;
+    /**
+     * interest earned.
+     */
     private double interestEarned;
 
+    /**
+     * Make a bank account.
+     *
+     * @param name            the name.
+     * @param accountCategory the category.
+     */
     public BankAccount(final String name, final BankAccountType accountCategory) {
-        /*
-         * Implement this function
-         */
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Null name provided.");
+        }
+        accountNumber = accountsCounter++;
+        this.ownerName = name;
+        this.setAccountType(accountCategory);
+        accountBalance = 0;
+        interestRate = 0; // replace with real default
     }
 
-    /*
-     * Implement getters and setters as appropriate for private variables.
+    /**
+     * @return the current number of accounts opened.
      */
+    public static int numberOfAccountsOpened() {
+        return accountsCounter;
+    }
+
+    /**
+     * account type.
+     *
+     * @return the account type.
+     */
+    public BankAccountType getAccountType() {
+        return accountType;
+    }
+
+    /**
+     * Set account type.
+     *
+     * @param type account type.
+     */
+    public void setAccountType(final BankAccountType type) {
+        this.accountType = type;
+    }
+
+    /**
+     * interest rate.
+     *
+     * @return the interest rate.
+     */
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    /**
+     * Set interest rate.
+     *
+     * @param ir interest Rate.
+     */
+    public void setInterestRate(final double ir) {
+        if (ir <= 0 || !Double.isFinite(ir)) {
+            final String s = "Account Interest Rate must be a POSITIVE FINITE NUMBER";
+            throw new IllegalArgumentException(s);
+        }
+        this.interestRate = ir;
+    }
+
+    /**
+     * Get the account number.
+     *
+     * @return the account number.
+     */
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * Get the current account balance.
+     *
+     * @return the current account balance.
+     */
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+    /* *@param number The new account number.* /
+    public void setAccountNumber(final int number) {
+        this.accountNumber = number;
+    }*/
+
+    /**
+     * Set the account's current balance.
+     *
+     * @param balance the new balance.
+     */
+    public void setAccountBalance(final double balance) {
+        if (balance < 0) {
+            throw new IllegalArgumentException("Overdrawn!");
+        }
+        this.accountBalance = balance;
+    }
+
+    /**
+     * Return the owner name.
+     *
+     * @return the owner name.
+     */
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    /**
+     * Set owner name.
+     *
+     * @param name new owner name -- must have at least one nonspace character!
+     */
+    public void setOwnerName(final String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Account Owner must have a name!");
+        }
+        this.ownerName = name;
+    }
+
+    /**
+     * Gets the current amount of earned interest.
+     *
+     * @return the current amount of earned interest.
+     */
+    public double getInterestEarned() {
+        return interestEarned;
+    }
+
+    /**
+     * Set the amount of interest earned.
+     *
+     * @param ie the amount of interest earned.
+     */
+    public void setInterestEarned(final double ie) {
+        if (ie < 0 || !Double.isFinite(ie)) {
+            final String s = "Interest Earned must be a NON-NEGATIVE FINITE NUMBER!";
+            throw new IllegalArgumentException(s);
+        }
+        this.interestEarned = ie;
+    }
+
+    /**
+     * Distinguishes between different kinds of accounts.
+     */
+    public enum BankAccountType {
+        /** A checking account. */
+        CHECKINGS,
+        /** A savings account. */
+        SAVINGS,
+        /** A student account. */
+        STUDENT,
+        /** A workplace account. */
+        WORKPLACE
+    }
 }
